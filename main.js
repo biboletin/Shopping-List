4b51ac2d33bd5b7aa98dd9b367f010a0a0b54283
biboletin <shady_fan@abv.bg>
2018-03-05 14:33:46 +0200

    DevTools functionality added

--- main.js
+++ main.js
@@ -7,17 +7,22 @@ const {app, BrowserWindow, Menu} = electron;
 let mainWindow;
 let addWindow;
 
-// listen for app to be ready 
+// listen for app to be ready
 app.on("ready", function(){
     // create new window
     mainWindow = new BrowserWindow({});
 
     // load html into window
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, "mainWindow.html"), 
-        protocol: "file:", 
+        pathname: path.join(__dirname, "mainWindow.html"),
+        protocol: "file:",
         slashes: true
     }));
+
+    // quit app when closed
+    mainWindow.on("closed", function(){
+        app.quit();
+    });
 
     // build menu from template
 
@@ -33,16 +38,20 @@ function createAddWindow(){
     // create new window
     addWindow = new BrowserWindow({
         width: 300,
-        height: 200, 
+        height: 200,
         title: "Add Shopping List Item"
     });
 
     // load html into window
     addWindow.loadURL(url.format({
-        pathname: path.join(__dirname, "addWindow.html"), 
-        protocol: "file:", 
+        pathname: path.join(__dirname, "addWindow.html"),
+        protocol: "file:",
         slashes: true
     }));
+    // garbage collection handle
+    addWindow.on("close", function(){
+        addWindow = null;
+    });
 }
 
 // create menu template
@@ -70,3 +79,28 @@ const mainMenuTemplate = [
         ]
     }
 ];
+
+// if mac, add empty object to menu
+
+if(process.platform == "darwin"){
+    mainMenuTemplate.unshift({});
+}
+
+// add developer tools item if not in production
+if (process.env.NODE_ENV !== "production") {
+    mainMenuTemplate.push({
+        label: "Developer Tools",
+        submenu: [
+            {
+                label: "Toggle DevTools",
+                accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I",
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: "reload"
+            }
+        ]
+    });
+}
