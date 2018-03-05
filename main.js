e0c733f0faa33edd4d8cb66e3f3a06a7740d0b24
biboletin <shady_fan@abv.bg>
2018-03-05 13:36:47 +0200

    added mainWindow.html

--- main.js
+++ main.js
@@ -4,4 +4,19 @@ const path = require("path");
 
 const {app, BrowserWindow} = electron;
 
-let mainWindow
+let mainWindow;
+
+// listen for app to be ready 
+app.on("ready", function(){
+    // create new window
+    mainWindow= new BrowserWindow({
+
+    });
+
+    // load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "mainWindow.html"), 
+        protocol: "file:", 
+        slashes: true
+    }));
+});
